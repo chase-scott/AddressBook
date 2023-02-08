b5366f70da92d4f52d6c4e0f601a267146795796
Chase <chasescott@cmail.carleton.ca>
2023-02-08 15:43:10 -0500

    Testing GitHub Actions

--- src/test/java/com/example/lab4/ApplicationTest.java
+++ src/test/java/com/example/lab4/ApplicationTest.java
@@ -16,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+
 @SpringBootTest
 @AutoConfigureMockMvc
 class ApplicationTest {
