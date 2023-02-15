a58e8cc3fd606af420b9e5caf8b532df7434bd4c
Chase <chasescott@cmail.carleton.ca>
2023-02-14 22:09:47 -0500

    changed project name

--- src/main/java/com/example/lab4/Lab6Application.java
+++ src/main/java/com/example/lab4/Lab6Application.java
@@ -10,10 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Lab4Application {
+public class Lab6Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Lab4Application.class, args);
+        SpringApplication.run(Lab6Application.class, args);
     }
 
     @Bean
